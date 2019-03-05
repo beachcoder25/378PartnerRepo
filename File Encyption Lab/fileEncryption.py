65f725d1ba539eb6f8cb85abccfb7eac63ac5735
beachcoder25 <cornish25@gmail.com>
2019-03-04 17:47:18 -0800

    Padding is working, message MUST BE 16 chars though (Block Size)

--- File Encyption Lab/fileEncryption.py
+++ File Encyption Lab/fileEncryption.py
@@ -2,6 +2,7 @@ import os
 import cryptography
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 
 # (C, IV)= Myencrypt(message, key):
 
@@ -18,16 +19,26 @@ def myEncrypt(message, key):
     # generate a 16 Bytes IV
     # IV is used so if we encrypt an identical piece of data that it 
     # comes out encrypted different each time its encrypted
-    
     backend = default_backend()
     IV = os.urandom(16)
 
+    # USE PKCS7 TO PAD!!!
+    # https://cryptography.io/en/latest/hazmat/primitives/padding/
+    padder = padding.PKCS7(128).padder()
+    padMessage = padder.update(message)
+    print(padMessage)
+
+    padMessage += padder.finalize()
+    print(padMessage)
+    
     
     cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)    #Cipher objects combine an algorithm such as AES with a mode like CBC
                                                                             # Notice we pass in the (key) to our AES argument, and (IV) to our CBC mode
     encryptor = cipher.encryptor()
     C = encryptor.update(message) + encryptor.finalize()                    # Cipher text = encypt message + finalize encryption
                                                                             # Message now encrypted
+    
+    
     print(C)
 
     # Decryption test
@@ -44,7 +55,17 @@ def myEncrypt(message, key):
 def main():
 
     key = os.urandom(32)
-    message = b"a secret message"
+    message1 = b"a secret message"
+    message = b"sixteen  letters"
+    
+
+    # padder = padding.PKCS7(128).padder()
+    # padMessage = padder.update(b"Hello")
+    # print(padMessage)
+
+    # padMessage += padder.finalize()
+    # print(padMessage)
+
 
     myEncrypt(message, key)
 
