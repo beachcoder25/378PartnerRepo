91445a4e67af59326b297d37a0db23589c90b3fc
beachcoder25 <cornish25@gmail.com>
2019-03-18 18:47:59 -0700

    Cleaned up, refactored, added constants for merging

--- File Encyption Lab/fileEncryption.py
+++ File Encyption Lab/fileEncryption.py
@@ -2,9 +2,7 @@ import os
 import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives import padding, hashes, hmac
 
 # Constants
 
