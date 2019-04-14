058c12bfa70cb79c74153580c82bdb0b6342964a
beachcoder25 <cornish25@gmail.com>
2019-04-13 21:21:32 -0700

    Re-Organized files

--- File Encyption Lab/Python File Encryption Files/fileEncryption.py
+++ File Encyption Lab/Python File Encryption Files/fileEncryption.py
@@ -3,6 +3,7 @@ import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hashes, hmac
+import json
 
 # Constants
 
@@ -12,6 +13,7 @@ HMAC_KEY = 16
 HMAC_KEY_LEN = len(str(HMAC_KEY))
 KEY_SIZE = 32
 ENC_KEY_SIZE = 32
+FILE_PATH = "C:/Users/corni/Desktop/ransomTest"
 
 
 # Encrypt message using AES, must pad message, and assign proper cipher mode (AES-CBC mode)
@@ -145,9 +147,211 @@ def myFileDecrypt(key, IV, inputFilepath):
     
     return M
 
+# Step1
+# Next, you will a script that looks for a pair of RSA Public and private key (using a CONSTANT file path; PEM format). 
+# If the files do not exist (use OS package) then generate the RSA public 
+# and private key (2048 bits length) using the same constant file path.
+
+# Finds first match
+
+def findKey(filePath):
+    # for root, dirs, files in os.walk(filePath):
+    #     if fileName in files:
+    #         print("Found: " + fileName)
+    #         return os.path.join(root, fileName)
+    privFound = 1
+    pubFound = 1
+
+    pubKey = "/public.pem"
+    privKey = "/private.pem"
+
+    pubFilePath = filePath + pubKey
+    privFilePath = filePath + privKey
+    pubExists = os.path.isfile(filePath)
+
+    # Check if public key exists
+
+    if pubExists:
+        pubFound = 1
+        print("Found public key!\nBoolean value: " + str(pubFound))
+        print(pubFilePath)
+
+    elif (pubExists == False):
+        pubFound = 0
+        print("Did not find public key!\nBoolean value: " + str(pubFound))
+
+    # Check if private key exists 
+
+    if pubExists:
+        pubFound = 1
+        print("Found public key!\nBoolean value: " + str(pubFound))
+        print(pubFilePath)
+
+    elif (pubExists == False):
+        pubFound = 0
+        print("Did not find public key!\nBoolean value: " + str(pubFound))
+
+    
+        
+        
+        
+
+    
+    # for root, dirs, files in os.walk(filePath):
+    #     for file in files:
+
+    #         if file.endswith("private.pem"):
+    #             privFound = 1
+    #             print("Found private key!\nBoolean value:  " + str(privFound))
+    #             print(os.path.join(root, file))
+
+    #         else:
+    #             privFound = 0
+    #             print("No private key found!\nBoolean value: " + str(privFound))
+
+            
+    #         if file.endswith("public.pem"):
+    #             pubFound = 1
+    #             print("Found public key!\nBoolean value: " + str(pubFound))
+    #             print(os.path.join(root, file))
+
+    #         else:
+    #             pubFound = 0
+    #             print("No public key found!\nBoolean value: " + str(pubFound))
+
+        
 
 
-def mainMAC():
+def fileFindTest():
+
+    
+    print("Running Test")
+    findKey(FILE_PATH)
+
+fileFindTest()
+
+def mainMACDesktop():
+    
+    #JSON Attempt
+    # JSONfile = "data.json"
+    data = {}
+    # filepath = "C:/Users/corni/Desktop/JSON.txt" # This the file you are trying to encrypt
+
+    desktopFilePath = "C:/Users/corni/Desktop/panda.jpg"
+    
+    
+    # Desktop
+    C, IV, tag, EncKey, HMACKey, ext = MyFileEncryptMAC(desktopFilePath)
+
+    # data[filepath] = { # Store in JSON so you can read out for decryption
+    #     "C" : str(C),
+    #     "IV" : str(IV),
+    #     "tag" : str(tag),
+    #     "EncKey": str(EncKey),
+    #     "HMACKey" : str(HMACKey),
+    #     "ext" : ext
+    # }
+
+    # Store encrypted data in a text file
+    print("Writing encrypted File")
+    
+    # DESKTOP
+    file = open("C:/Users/corni/Desktop/testfile.txt","wb") # wb for writing in binary mode 
+
+    # LAPTOP
+    #file = open("C:/Users/Jonah/Desktop/378Lab/testfile.txt","wb") # wb for writing in binary mode
+    
+    file.write(C) # Writes cipher byte-message into text file
+    file.close() 
+   
+    # Desktop
+    encryptedFilepath = "C:/Users/corni/Desktop/testfile.txt"
+
+    # Laptop
+    # encryptedFilepath = "C:/Users/Jonah/Desktop/378Lab/testfile.txt"
+
+
+    # Message verification
+    print("\nVerifying message:")
+    h = hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend())
+    h.update(C)
+    receiverTag = h.finalize()
+
+    M = MydecryptMAC(C, IV, tag, EncKey, HMACKey, encryptedFilepath)
+
+    # data[filepath] = {
+    #     "C" : str(C),
+    #     "IV" : str(IV),
+    #     "tag" : str(tag),
+    #     "EncKey": str(EncKey),
+    #     "HMACKey" : str(HMACKey),
+    #     "ext" : ext
+    # }
+
+    # Write to JSON file
+
+    
+    # s = json.dumps(data)
+    # with open("data.json", "w") as fp:
+    #     json.dump(s, fp)
+        
+    print("Writing decrypted File")
+
+    # DESKTOP
+    file = open("C:/Users/corni/Desktop/outputfileYYY.jpg","wb") # wb for writing in binary mode
+
+    # LAPTOP
+    #file = open("C:/Users/Jonah/Desktop/378Lab/outputfile.jpg","wb") # wb for writing in binary mode
+    
+    file.write(M) # Writes cipher byte-message into text file
+    file.close() 
+
+
+
+
+
+
+
+def mainNoMAC():
+
+    # File path for desktop
+    desktopFilePath = "C:/Users/corni/Desktop/panda.jpg"
+ 
+    C, IV, key, ext = myFileEncrypt(desktopFilePath)
+
+    # Store encrypted data in a text file
+    print("Writing encrypted File")
+    
+    # DESKTOP
+    file = open("C:/Users/corni/Desktop/testfile.txt","wb") # wb for writing in binary mode 
+
+    # LAPTOP
+    #file = open("C:/Users/Jonah/Desktop/378Lab/testfile.txt","wb") # wb for writing in binary mode
+    
+    file.write(C) # Writes cipher byte-message into text file
+    file.close() 
+   
+    # Desktop
+    encryptedFilepath = "C:/Users/corni/Desktop/testfile.txt"
+
+    # Laptop
+    # encryptedFilepath = "C:/Users/Jonah/Desktop/378Lab/testfile.txt"
+
+    # Decyption
+    M = myFileDecrypt(key, IV, encryptedFilepath)
+    print("Writing decrypted File")
+
+    # DESKTOP
+    file = open("C:/Users/corni/Desktop/outputfile.jpg","wb") # wb for writing in binary mode
+
+    # LAPTOP
+    #file = open("C:/Users/Jonah/Desktop/378Lab/outputfile.jpg","wb") # wb for writing in binary mode
+    
+    file.write(M) # Writes cipher byte-message into text file
+    file.close() 
+    
+
+def mainMACLaptop():
     
      # File path for desktop
     desktopFilePath = "C:/Users/corni/Desktop/panda.jpg"
@@ -199,59 +403,7 @@ def mainMAC():
     file.write(M) # Writes cipher byte-message into text file
     file.close() 
 
-
-
-mainMAC()
-
-
-
-def mainNoMAC():
-
-    # File path for desktop
-    desktopFilePath = "C:/Users/corni/Desktop/panda.jpg"
-    laptopFilePath = "C:/Users/Jonah/Desktop/378Lab/---.jpg"
-
-    # File encryption (NON-MAC)
-    
-    # Desktop
-    C, IV, key, ext = myFileEncrypt(desktopFilePath)
-
-    # Laptop
-    # C, IV, key, ext = myFileEncrypt(laptopFilePath)
-
-    # Store encrypted data in a text file
-    print("Writing encrypted File")
-    
-    # DESKTOP
-    file = open("C:/Users/corni/Desktop/testfile.txt","wb") # wb for writing in binary mode 
-
-    # LAPTOP
-    #file = open("C:/Users/Jonah/Desktop/378Lab/testfile.txt","wb") # wb for writing in binary mode
-    
-    file.write(C) # Writes cipher byte-message into text file
-    file.close() 
-   
-    # Desktop
-    encryptedFilepath = "C:/Users/corni/Desktop/testfile.txt"
-
-    # Laptop
-    # encryptedFilepath = "C:/Users/Jonah/Desktop/378Lab/testfile.txt"
-
-    # Decyption
-    M = myFileDecrypt(key, IV, encryptedFilepath)
-    print("Writing decrypted File")
-
-    # DESKTOP
-    file = open("C:/Users/corni/Desktop/outputfile.jpg","wb") # wb for writing in binary mode
-
-    # LAPTOP
-    #file = open("C:/Users/Jonah/Desktop/378Lab/outputfile.jpg","wb") # wb for writing in binary mode
-    
-    file.write(M) # Writes cipher byte-message into text file
-    file.close() 
-    
-
-    
+# mainMACDesktop()
 
 # if __name__ == '__main__':
 #     main()
