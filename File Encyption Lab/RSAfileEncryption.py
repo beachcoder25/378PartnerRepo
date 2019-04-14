a9f4efde547e2e3c1788c3115114f519b8a5a60b
beachcoder25 <cornish25@gmail.com>
2019-04-13 21:55:13 -0700

    Serializing keys

--- File Encyption Lab/RSAfileEncryption.py
+++ File Encyption Lab/RSAfileEncryption.py
@@ -1,8 +1,14 @@
 import os
+
+# May not need
 import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding, hashes, hmac, serialization
+
+# Definitely need
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding, hashes, hmac
+
 import json
 
 # Constants
@@ -14,7 +20,7 @@ PRIV_FOUND = False
 
 
 
-def findKey(filePath):
+def findKeys(filePath):
     
     privFound = 1
     pubFound = 1
@@ -28,6 +34,26 @@ def findKey(filePath):
     pubExists = os.path.isfile(pubFilePath)
     privExists = os.path.isfile(privFilePath)
 
+
+    # Check if private key exists 
+
+    if privExists:
+        PRIV_FOUND = 1
+        print("\nFound public key! \nFilePath: " + privFilePath)
+        
+
+    elif (pubExists == False):
+        PRIV_FOUND = 0
+        print("\nDid not find public key!\nBoolean value: " + str(PRIV_FOUND))     
+
+        # generate private key
+
+        private_key = rsa.generate_private_key(public_exponent = 65537, key_size = 2048, backend = default_backend()) 
+
+        # Serialize key 
+
+        private_pem = private_key.private_bytes(encoding = serialization.Encoding.PEM, format = serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm = serialization.NoEncryption())
+    
     # Check if public key exists
 
     if pubExists:
@@ -39,16 +65,14 @@ def findKey(filePath):
         PUB_FOUND = 0
         print("Did not find public key!\nBoolean value: " + str(PUB_FOUND))
 
-    # Check if private key exists 
+        # generate private key
 
-    if privExists:
-        PRIV_FOUND = 1
-        print("\nFound public key! \nFilePath: " + privFilePath)
+
+        
         
 
-    elif (pubExists == False):
-        PRIV_FOUND = 0
-        print("\nDid not find public key!\nBoolean value: " + str(PRIV_FOUND))      
+    
+
 
     print(" ")  
 
@@ -57,7 +81,7 @@ def fileFindTest():
 
     
     print("Running Test")
-    findKey(FILE_PATH)
+    findKeys(FILE_PATH)
 
 fileFindTest()
 
