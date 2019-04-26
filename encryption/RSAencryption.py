681adb744b026c6bdad38eb1c4e20fa450a5731b
rgabeflores <rgabeflores@gmail.com>
2019-04-25 23:48:21 -0700

    Setting up requests for server API calls

--- encryption/RSAencryption.py
+++ encryption/RSAencryption.py
@@ -5,8 +5,10 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from getpass import getpass
 from os import walk, getcwd, listdir, remove
 from os.path import isfile, join
-from json import dump, dumps
+from json import dump, dumps, loads
 from sys import argv
+
+from requests import get, post  # requests module is better than urllib
 
 from handlers import cd, getAllFiles
 from encryption import myFileEncryptMAC, myFileDecryptMAC
@@ -28,11 +30,14 @@ from encryption import myFileEncryptMAC, myFileDecryptMAC
         - must be able to describe weaknesses of implementation
 
     OTHER-BONUS:
-        - write a script to submit multiple votes 
+        - write a script to submit multiple votes
 '''
 
 # Debug flag for testing purposes
 DEBUG = True
+
+# API Key to access Node server's endpoints
+API_KEY = ''
 
 # Get directory of script
 CWD = getcwd()
@@ -47,6 +52,7 @@ RSA_PUBLIC_KEY_PATH = f'{CWD}/keys/public_key.pem'
 # Default backend used for cryptography libraries
 BACKEND = default_backend()
 
+
 def myRSAEncrypt(filepath, RSA_Publickey_filepath=RSA_PUBLIC_KEY_PATH):
     '''
         Encrypts the file at the given path using the RSA encryption method with OAEP padding.
@@ -60,14 +66,15 @@ def myRSAEncrypt(filepath, RSA_Publickey_filepath=RSA_PUBLIC_KEY_PATH):
     except FileNotFoundError as e:
         print('A public key was missing at the specified path.')
         exit(1)
-    
+
     # Concatenate encryption key and HMAC key to serve as the RSA encryption input
     RSAInput = Enckey + HMACKey
 
     # Use the public key object to encrypt the data
-    RSACipher = public_key.encrypt(RSAInput, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),label=None))
+    RSACipher = public_key.encrypt(RSAInput, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
 
     return (RSACipher, C, IV, tag, ext)
+
 
 def myRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_filepath=RSA_PRIVATE_KEY_PATH):
     '''
@@ -78,7 +85,7 @@ def myRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_filepath=RSA_PRIVATE
         with open(RSA_Privatekey_filepath, 'rb') as key_file:
             private_key = serialization.load_pem_private_key(key_file.read(), password=RSA_PRIVATE_KEY_PASSWORD, backend=BACKEND)
     except TypeError as e:
-        # serialization.load_pem_private_key raises a TypeError for issues with password argument 
+        # serialization.load_pem_private_key raises a TypeError for issues with password argument
         # Try loading the .pem file without a password
         with open(RSA_Privatekey_filepath, 'rb') as key_file:
             private_key = serialization.load_pem_private_key(key_file.read(), backend=BACKEND)
@@ -88,7 +95,7 @@ def myRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_filepath=RSA_PRIVATE
 
     # Use the private key object to decrypt
     result = private_key.decrypt(RSACipher, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-    
+
     # Both encryption key and HMAC key are expected to be the same size, therefore split the decrypted data directly in half
     Enckey, HMACKey = result[:len(result) / 2], result[len(result) / 2:]
 
@@ -96,6 +103,7 @@ def myRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_filepath=RSA_PRIVATE
     # M = myFileDecryptMAC(filename, Enckey, HMACKey, IV, tag)
 
     return (Enckey, HMACKey)
+
 
 def generateKeyPair(private_key_path, public_key_path, public_exponent=65537, key_size=2048, backend=BACKEND, password_required=True):
     '''
@@ -105,13 +113,13 @@ def generateKeyPair(private_key_path, public_key_path, public_exponent=65537, ke
 
     if password_required:
         print('Enter a password for the private key:')
-        key_passphrase = getpass() # Hide text while password is entered
+        key_passphrase = getpass()  # Hide text while password is entered
         encryption_algorithm = serialization.BestAvailableEncryption(key_passphrase)
     else:
         encryption_algorithm = serialization.NoEncryption()
 
     # Generate RSA private key
-    private_key = rsa.generate_private_key(public_exponent=public_exponent,key_size=key_size,backend=backend)
+    private_key = rsa.generate_private_key(public_exponent=public_exponent, key_size=key_size, backend=backend)
 
     # Save the private key
     with open(private_key_path, 'wb') as f:
@@ -130,13 +138,20 @@ def generateKeyPair(private_key_path, public_key_path, public_exponent=65537, ke
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
-    
+
     return key_passphrase
 
 
 def main():
 
-    global RSA_PRIVATE_KEY_PASSWORD
+    global RSA_PRIVATE_KEY_PASSWORD, API_KEY
+
+    try:
+        # Load the Node server API key from the config file
+        with open('./config/config.json', 'r') as f:
+            API_KEY = loads(''.join(f.readlines()))['API_KEY']
+    except Exception as e:
+        API_KEY = 'Dgfsi7VcOmB7klQJJOdk'
 
     # Check if a .pem file for private key exists
     if not(isfile(RSA_PRIVATE_KEY_PATH)):
@@ -158,13 +173,12 @@ def main():
         if int(input('Continue? [1] Yes [2] No\n')) == 2:
             exit(0)
 
-
     for target_file in target_files:
         # Initialize JSON output as dictionary
         output = dict()
 
         # NOTE: Can be written in one line
-        RSACipher, C, IV, tag, ext = myRSAEncrypt(target_file) 
+        RSACipher, C, IV, tag, ext = myRSAEncrypt(target_file)
         output['RSACipher'] = RSACipher
         output['C'] = C
         output['IV'] = IV
@@ -174,12 +188,13 @@ def main():
         # TO-DO: Need to solve encoding
         with open(f'{target_file}-out.json', 'w') as fp:
             dump(output, fp)
-        
+
         # # Delete original file using os module
         # remove(target_file)
 
     for result in results:
         pass
+
 
 if __name__ == '__main__':
     main()
